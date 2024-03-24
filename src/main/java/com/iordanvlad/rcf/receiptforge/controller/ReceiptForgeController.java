5b15447ab11513f8daaf76f310258892cfd821e9
Vlad <iordanvlad36@gmail.com>
2024-03-24 12:22:08 +0200

    initial commit of the receipt forge

---
+++ src/main/java/com/iordanvlad/rcf/receiptforge/controller/ReceiptForgeController.java
@@ -0,0 +1,28 @@
+package com.iordanvlad.rcf.receiptforge.controller;
+
+import com.iordanvlad.rcf.receiptforge.outputmodels.Receipt;
+import com.iordanvlad.rcf.receiptforge.service.ReceiptForgeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/generator")
+public class ReceiptForgeController {
+
+    private final ReceiptForgeService receiptForgeService;
+
+    @Autowired
+    public ReceiptForgeController (ReceiptForgeService receiptForgeService) {
+        this.receiptForgeService = receiptForgeService;
+    }
+
+    @GetMapping("/get-receipt")
+    public ResponseEntity<Receipt> getMockReceipt () {
+        return ResponseEntity.ok(receiptForgeService.getReceipt());
+
+    }
+}
+
---
+++ src/test/java/com/iordanvlad/rcf/receiptforge/ReceiptForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iordanvlad.rcf.receiptforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
